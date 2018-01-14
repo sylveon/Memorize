8300cb051859d78a6be7f8549d943ffa445fd314
Charles Milette <charles.milette@gmail.com>
2018-01-14 12:45:33 -0500

    Use dictionary built-ins when possible

--- Memorizer.cs
+++ Memorizer.cs
@@ -6,12 +6,14 @@ namespace Sylveon.Memorize
     public class Memorizer<T, TResult>
     {
         private Func<T, TResult> _functionToMemorize;
-
         private Dictionary<T, TResult> _memorizedResults = new Dictionary<T, TResult>();
 
         public Memorizer(Func<T, TResult> func) => this._functionToMemorize = func;
 
         public bool IsResultMemorized(T param) => this._memorizedResults.ContainsKey(param);
+        public void ClearMemorizedResults() => this._memorizedResults.Clear();
+        public TResult GetMemorizedResult(T param) => this._memorizedResults[param];
+        public bool TryGetMemorizedResult(T param, out TResult output) => this._memorizedResults.TryGetValue(param, out output);
 
         public TResult Invoke(T param)
         {
@@ -21,21 +23,5 @@ namespace Sylveon.Memorize
             }
             return this._memorizedResults[param];
         }
-
-        public bool GetMemorizedResult(T param, out TResult output)
-        {
-            if(IsResultMemorized(param))
-            {
-                output = this._memorizedResults[param];
-                return true;
-            }
-            else
-            {
-                output = default;
-                return false;
-            }
-        }
-
-        public void ClearMemorizedResults() => this._memorizedResults.Clear();
     }
 }
